8dd4fb5317a2ea59d2df59d92dd65643f346b69c
Irere Emmy <irere2020@outlook.com>
2023-09-30 14:48:25 +0200

    initial commit

---
+++ contentlayer.config.js
@@ -0,0 +1,21 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+
+const Blog = defineDocumentType(() => ({
+  name: "Blog",
+  filePathPattern: `**/*.mdx`,
+  fields: {
+    title: { type: "string", required: true },
+    date: { type: "string", required: true },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (post) => `/posts/${post._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: "content",
+  documentTypes: [Blog],
+});
