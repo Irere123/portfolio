ead6c90c2db5f1f522ab8ae7302624a2e2c1d70b
Irere123 <irere2020@outlook.com>
2024-04-09 17:26:16 +0200

    fix: remove blog link export

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,5 @@
-import { ArrowIcon } from "@/components/ArrowIcon";
-import { getBlogPosts } from "@/db/blog";
-import { Suspense } from "react";
-import ViewCounter from "./blog/view-counter";
-import { getViewCount } from "@/db/queries";
 import { Navbar } from "@/components/navbar";
 import AboutMe from "@/components/about-me";
-
-interface BlogLinkProps {
-  slug: string;
-  name: string;
-  summary: string;
-}
-
-async function Views({ slug }: { slug: string }) {
-  let views = await getViewCount();
-  return <ViewCounter allViews={views} slug={slug} />;
-}
-
-export async function BlogLink({ slug, name, summary }: BlogLinkProps) {
-  return (
-    <a
-      href={`/blog/${slug}`}
-      className="border bg-neutral-50 dark:bg-background drak:border rounded flex items-center justify-between px-3 py-4 w-full"
-    >
-      <div className="flex flex-col">
-        <p className="font-bold text-neutral-900 dark:text-foreground">
-          {name}
-        </p>
-        <p className="dark:text-muted-foreground">{summary}</p>
-        <Suspense fallback={<p className="h-6" />}>
-          <Views slug={slug} />
-        </Suspense>
-      </div>
-      <div className="text-neutral-700 ">
-        <ArrowIcon />
-      </div>
-    </a>
-  );
-}
 
 export default function Home() {
   return (
