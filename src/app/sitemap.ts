9d4a3bd382431b8c0c14308c721fa2f0bb86c715
Irere123 <irere2020@outlook.com>
2024-04-14 20:01:26 +0200

    feat: add title, content and description to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,15 @@
 import { getBlogPosts } from "@/db/blog";
 
 export default async function sitemap() {
-  const blogs = getBlogPosts().map((post) => ({
-    url: `https://irere.vercel.app/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }));
+  const blogs = getBlogPosts()
+    .filter((post) => post.metadata.published == "true")
+    .map((post) => ({
+      url: `https://irere.vercel.app/blog/${post.slug}`,
+      lastModified: post.metadata.publishedAt,
+      description: post.content,
+      title: post.metadata.title,
+      summary: post.metadata.summary,
+    }));
 
   const routes = ["", "/blog"].map((route) => ({
     url: `https://irere.vercel.app${route}`,
